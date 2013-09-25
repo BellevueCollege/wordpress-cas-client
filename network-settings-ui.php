c197f91303796324850658a40887279eaf89cdfb
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2013-09-25 10:22:34 -0700

    Updated plugin form setting fields.
    Resolved #19. Merged CAS parameters (Hostname, Port, path) into one field.
    Wrote a parsing function to parse the new field. Same for LDAP parameters.
    
    Conflicts:
    	wordpress-cas-client.php

--- network-settings-ui.php
+++ network-settings-ui.php
@@ -2,17 +2,20 @@
 	function updateNetworkSettings()
 	{
 		//saving CAS network settings
-		if(isset($_POST['wpcasldap_server_hostname']))
+		if(isset($_POST['wpcasldap_include_path']))
 		{
 			$cas_version = $_POST['wpcasldap_cas_version'];
 			$include_path = $_POST['wpcasldap_include_path'];
-			$server_hostname = $_POST['wpcasldap_server_hostname'];
-			$server_port = $_POST['wpcasldap_server_port'];
-			$server_path = $_POST['wpcasldap_server_path'];
+			$casserver = $_POST['wpcasldap_casserver'];
+			error_log("cas server url :".$casserver);
+			//$server_hostname = $_POST['wpcasldap_server_hostname'];
+			//$server_port = $_POST['wpcasldap_server_port'];
+			//$server_path = $_POST['wpcasldap_server_path'];
 			$useradd = $_POST['wpcasldap_useradd'];
 			$userrole = $_POST['wpcasldap_userrole'];
-			$ldaphost = $_POST['wpcasldap_ldaphost'];
-			$ldapport = $_POST['wpcasldap_ldapport'];
+			//$ldaphost = $_POST['wpcasldap_ldaphost'];
+			//$ldapport = $_POST['wpcasldap_ldapport'];
+			$ldapuri = $_POST['wpcasldap_ldapuri'];
 			$useldap = $_POST['wpcasldap_useldap'];
 			$ldapbasedn = $_POST['wpcasldap_ldapbasedn'];	
 			$ldapuser = $_POST['wpcasldap_ldapuser'];	
@@ -20,13 +23,15 @@
 
 			 update_site_option('wpcasldap_cas_version',$cas_version);
 			 update_site_option('wpcasldap_include_path',$include_path);
-			 update_site_option('wpcasldap_server_hostname',$server_hostname);
-			 update_site_option('wpcasldap_server_port',$server_port);
-			 update_site_option('wpcasldap_server_path',$server_path);
+			  update_site_option('wpcasldap_casserver',$casserver);
+			 //update_site_option('wpcasldap_server_hostname',$server_hostname);
+			 //update_site_option('wpcasldap_server_port',$server_port);
+			 //update_site_option('wpcasldap_server_path',$server_path);
 			 update_site_option('wpcasldap_useradd',$useradd);
 			 update_site_option('wpcasldap_userrole',$userrole);
-			 update_site_option('wpcasldap_ldaphost',$ldaphost);
-			 update_site_option('wpcasldap_ldapport',$ldapport);
+			 update_site_option('wpcasldap_ldapuri',$ldapuri);
+			 //update_site_option('wpcasldap_ldaphost',$ldaphost);
+			 //update_site_option('wpcasldap_ldapport',$ldapport);
 			 update_site_option('wpcasldap_useldap',$useldap);
 			 update_site_option('wpcasldap_ldapbasedn',$ldapbasedn);
 			 update_site_option('wpcasldap_ldapuser',$ldapuser);
