278a3de56b3e69ebac7e7a0cdd750fbe5f76b000
Shawn South <shawn.south@bellevuecollege.edu>
2013-09-29 18:27:24 -0700

    Moved string literal to a constant.

--- utilities.php
+++ utilities.php
@@ -7,13 +7,35 @@
  * To change this template use File | Settings | File Templates.
  */
 
+// Disable to turn off debug logging
+define("ENABLE_DEBUG_LOG", true);
+define("DEBUG_LOG_PATH", "/var/tmp/wordpress-cas-client-debug.log");
+
+/**
+ * @param $haystack
+ * @param $needle
+ *
+ * @return bool
+ */
+function str_starts_with($haystack, $needle)
+{
+  $idx = stripos($haystack, $needle);
+
+  return (false !== $idx && 0 == $idx);
+}
+
+/**
+ * @param $message
+ */
 function debug_log($message)
 {
-    $debug_log_path = "/var/tmp/wordpress-cas-client-debug.log";
-    if(!error_log($message."\n", 3, $debug_log_path))
+  if (ENABLE_DEBUG_LOG)
+  {
+    if (!error_log($message . "\n", 3, DEBUG_LOG_PATH))
     {
-        error_log("UNABLE TO WRITE TO DEBUG LOG (".$debug_log_path."): '".$message."'");
+      error_log("UNABLE TO WRITE TO DEBUG LOG (" . DEBUG_LOG_PATH . "): '" . $message . "'");
     }
+  }
 }
 
 ?>
