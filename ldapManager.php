ae0f15f93291bdd2b6003f7e5b7bcc7b8a5bf2a3
Shawn South <shawn.south@bellevuecollege.edu>
2013-10-01 20:01:35 -0700

    Encapsulating common code into classes
    Began the work of refactoring ldap and cas objects into classes so they
    can be managed atomically. WARNING: This code contains errors.

--- ldapManager.php
+++ ldapManager.php
@@ -8,6 +8,8 @@
  */
 
 include_once(dirname(__FILE__) . "/utilities.php");
+include_once(dirname(__FILE__) . "/ldapUser.php");
+
 
 /**
  * Class ldapManager
@@ -43,6 +45,11 @@ class ldapManager
    */
   private $connection = null;
 
+  /**
+   * @var string
+   */
+  private $password = "";
+
   //  unset properties
   /**
    * The URL to use for LDAP connections.
@@ -50,6 +57,43 @@ class ldapManager
    * @var string
    */
   public $Uri = "";
+
+  /**
+   * @var string
+   */
+  public $Username = "";
+
+  /**
+   * @param        $login
+   * @param        $password
+   * @param string $uri
+   */
+  function __construct($login = "", $password = "", $uri = "")
+  {
+    if($login != "") {
+      error_log("LDAP username cannot be blank. (contstructor)");
+      return null;
+    }
+    if($password == "") {
+      error_log("LDAP password cannot be blank. (contstructor)");
+      return null;
+    }
+
+    $this->Username = $login;
+    $this->password = $password;
+
+    if($uri != "") {
+      $this->Uri = $uri;
+    }
+  }
+
+  /**
+   *
+   */
+  function __destruct()
+  {
+    $this->Close();
+  }
 
   // interface methods
   /**
@@ -61,20 +105,7 @@ class ldapManager
    */
   public function Connect($uri = "")
   {
-    if ($uri == "")
-    {
-      debug_log("No hostname provided to Connect(), falling back on Uri property.");
-      if ($this->Uri == "")
-      {
-        error_log("Unable to connect - no LDAP URI was provided.");
-        return null;
-      }
-      $uri = $this->Uri;
-    }
-    else
-    {
-      $this->Uri = $uri;
-    }
+    if(!$this->HaveUri($uri)) { return null; }
     debug_log("Establishing LDAP connection: " . $uri . "...");
 
     try
@@ -222,6 +253,152 @@ class ldapManager
     return $components;
   }
 
+  /**
+   * @param        $uid
+   *
+   * @param string $baseDN
+   *
+   * @return bool|ldapUser
+   */
+  function GetUser($uid, $baseDN)
+  {
+    if(!$this->HaveUri()) { return null; }
+
+    try
+    {
+      $ds = $this->Connect($this->Uri);
+      if (!$ds) {
+        error_log("\n" . 'Error in contacting the LDAP server.');
+      } else {
+        //error_log("\n".$filter);
+        /*
+        $ldap_dn = $wpcasldap_use_options['ldapbasedn'];
+        */
+        //echo "<h2>Connected</h2>";
+
+        // Make sure the protocol is set to version 3
+        if (!$this->SetOption(ldapManager::OPT_PROTOCOL_VERSION, 3)) {
+          error_log("\n" . 'Failed to set protocol version to 3.');
+        } else {
+          //Connection made -- bind anonymously and get dn for username.
+          debug_log("username :" . $this->Username);
+          debug_log("password :" . $this->password);
+          //echo "ldap user :".$ldaprdn ;
+          if(empty($ldaprdn) || empty($ldappass))
+          {
+            echo "ERROR: LDAP Username or LDAP Password not configured correctly";
+            exit();
+          }
+
+          $bind = $this->Bind($this->Username, $this->password);
+          //$bind = @ldap_bind($ds);
+          //Check to make sure we're bound.
+          if (!$bind) {
+            $error = 'Binding to LDAP failed.';
+            echo "\nERROR: " . $error;
+            error_log($error);
+            //exit();
+          } else {
+
+            /*
+            This code is added to get all the groups a users belongs to.
+            */
+            /*
+            $GroupsDN = array();
+            $filter = "sAMAccountName=".$uid;
+            $attributes_ad = array("dn","givenName","sn","primaryGroupID");
+            //Query to get Primary group id
+            $search = ldap_search($ds, $wpcasldap_use_options['ldapbasedn'], $filter,$attributes_ad);
+            $result = ldap_get_entries($ds, $search);
+            error_log( "result:".print_r($result,true));
+
+            $pri_grp_rid = $result[0]['primarygroupid'][0];
+            echo "primaryGroupID :".$pri_grp_rid ;
+
+            $r = ldap_read($ds, $wpcasldap_use_options['ldapbasedn'], '(objectclass=*)', array('objectSid')) or exit('ldap_search');
+            $data = ldap_get_entries($ds, $r);
+            $domain_sid = $data[0]['objectsid'][0];
+            echo "<br/> domain sid:".$domain_sid;
+            $domain_sid_s = sid2str($domain_sid);
+            echo "<br/> domain sid s:".$domain_sid_s;
+            //Request to get Primary group CN
+            $r = ldap_search($ds, $wpcasldap_use_options['ldapbasedn'], "objectSid=${domain_sid_s}-${pri_grp_rid}", array('cn')) or exit('ldap_search');
+            $data = ldap_get_entries($ds, $r);
+            error_log("\n data:".print_r($data,true));
+            //exit();
+
+            $defaultGroupDN = $data[0]['dn'];
+            $getCN = $data[0]['cn'][0];
+            //$defaultGroupDN = "CN=".$getCN.",OU=Groups,DC=BellevueCollege,DC=EDU" ;
+            echo "<br/> CN:".$getCN;
+
+
+            echo("<br/> dn = ".$defaultGroupDN."\n");
+            // This query is to get all the groups which are memberOf Primary Group
+            //Its not working right now.
+            if($defaultGroupDN !=null)
+            {
+                $GroupsDN[] = $defaultGroupDN ;
+                $filter = "(memberof:1.2.840.113556.1.4.1941:=".$defaultGroupDN.")";
+                $attributes_ad = array("CN");
+                $search = ldap_search($ds, $wpcasldap_use_options['ldapbasedn'], $filter,$attributes_ad);
+                $info = ldap_get_entries($ds, $search);
+                echo("<br/>".print_r($info,true));
+                for($i=0;$i<count($info);$i++)
+                {
+
+                        if($info[$i]["dn"] !=null)
+                            $GroupsDN[] = $info[$i]["dn"] ;
+                        echo(print_r("<br/>".$info[$i]["dn"],true) ."<br/>");
+
+                    //var_dump($info[$i]);
+                }
+            }
+
+            if($result[0]["dn"] !=null)
+            {
+                $filter = "(member:1.2.840.113556.1.4.1941:=".$result[0]["dn"].")";
+                $attributes_ad = array("CN");
+                $search = ldap_search($ds, $wpcasldap_use_options['ldapbasedn'], $filter,$attributes_ad);
+                $info = ldap_get_entries($ds, $search);
+
+                //error_log("\nresult identifier :".$info);
+                error_log("\nenterries :".print_r($info,true));
+                echo "count :".count($info);
+                for($i=0;$i<count($info);$i++)
+                {
+
+                        if($info[$i]["dn"] !=null)
+                            $GroupsDN[] = $info[$i]["dn"] ;
+                        echo(print_r($info[$i]["dn"],true) ."<br/>");
+
+                    //var_dump($info[$i]);
+                }
+                //var_dump($info);
+                exit();
+            }
+
+            */
+            $search = $this->Search($baseDN, "sAMAccountName=$uid", array('uid','mail','givenname','sn','rolename','cn','EmployeeID','sAMAccountName'));
+            $info = $this->GetSearchResults($search);
+
+            $this->Close();
+            // TODO: Is this code assuming that $info only contains one record?
+            return new ldapUser($info);
+          }
+          $this->Close();
+        }
+      }
+    }
+    catch (Exception $e)
+    {
+      $err_msg = "An LDAP error occurred while talking to '" . $this->Uri . "': " . $e->getMessage();
+      error_log($err_msg);
+      debug_log($err_msg);
+    }
+    return FALSE;
+  }
+
   // Private methods
 
   /**
@@ -263,5 +440,28 @@ class ldapManager
   {
     $hostpath = empty($uri_parts["host"]) ? $uri_parts["path"] : $uri_parts["host"] . (empty($uri_parts["path"]) ? "" : $uri_parts["path"]);
     return $scheme . "://" . $hostpath;
+  }
+
+  /**
+   * @param $uri
+   * @return bool
+   */private function HaveUri($uri = "")
+  {
+    if ($uri != "")
+    {
+      $this->Uri = $uri;
+    }
+    else
+    {
+      debug_log("No URL provided, falling back on Uri property.");
+      if ($this->Uri != "")
+      {
+        return true;
+      }
+      else
+      {
+        error_log("Unable to continue - no LDAP URI was provided.");
+      }
+    }
   }
 }
