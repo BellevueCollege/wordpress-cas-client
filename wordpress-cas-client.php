4d1130f94c64ff4c8967490efeaf379552edb436
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2014-05-07 13:34:13 -0700

    Add Function generate_password
    Add function generate_password to create cryptographically secure pseudo
    random passwords for WordPress account creations.

--- wordpress-cas-client.php
+++ wordpress-cas-client.php
@@ -254,6 +254,58 @@ class WP_CAS_LDAP {
 }
 
 /**
+ * Modulo function that works with negative numbers
+ *
+ * PHP's standard modulo operator returns negative numbers as is without
+ * modulus operation (ie -3 % 7 == -3 not the expected 3 % 7 == 4). This
+ * function returns the expected negative module operation on negative numbers.
+ *
+ * @param int $num
+ * @param int $mod
+ * @return int
+ */
+function true_modulo( $num, $mod ) {
+	return ( $mod + ( $num % $mod ) ) % $mod;
+}
+
+/**
+ * Generate secure passwords of random length
+ *
+ * Generate passwords that have printable keyboard characters that are a
+ * random length between the $min and $max paramters passed to the function.
+ *
+ * @param int $min the minimum length of the random password to generate
+ * @param int $max the maximum length of the random password ro generate
+ * @return string a random password that is a random length between the $min
+ *                and $max paramter values
+ */
+function generate_password( $min, $max ) {
+	$password = '';
+
+	/*
+	 * Generate cryptographically strong pseudo-random bytes as $bytes using the
+	 * openssl_random_pseudo_bytes fuction. Number of bytes generated determined
+	 * by $min and $max variables passed to this function.
+	 */
+	do {
+		$bytes = openssl_random_pseudo_bytes( mt_rand( $min, $max ), $crypto_strong );
+	} while ( false === $crypto_strong );
+
+	/*
+	 * Iterate through $bytes one byte at a time and modulus each byte's decimal
+	 * value to be between 33 and 126 for valid keyboard characters on the ASCII
+	 * table.
+	 */
+	foreach ( str_split( $bytes ) as $byte ) {
+		$integer_value = ord( $byte );
+		$integer_value = true_modulo( ( $integer_value - 33 ), 94 ) + 33;
+		$password .= chr( $integer_value );
+	}
+
+	return $password;
+}
+
+/**
  * wp_cas_ldap_now_puser function
  *
  * @param string $new_user_id the username of a user
