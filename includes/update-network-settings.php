d0ae1c55fcad0644d781f3006b33b07b82086578
Benjamin Renard <brenard@easter-eggs.com>
2019-01-23 15:14:41 +0100

    Fix update network settings

--- includes/update-network-settings.php
+++ includes/update-network-settings.php
@@ -32,17 +32,28 @@
 function update_network_settings( ) {
 	//saving CAS network settings
 	if ( isset( $_POST['wpcasldap_server_hostname'] ) ) {
-		$cas_version     = $_POST['wpcasldap_cas_version'];
-		$include_path    = $_POST['wpcasldap_include_path'];
-		$server_hostname = $_POST['wpcasldap_server_hostname'];
-		$server_port     = $_POST['wpcasldap_server_port'];
-		$server_path     = $_POST['wpcasldap_server_path'];
-		$user_add        = $_POST['wpcasldap_useradd'];
-		$user_role       = $_POST['wpcasldap_userrole'];
-		$ldap_host       = $_POST['wpcasldap_ldaphost'];
-		$ldap_port       = $_POST['wpcasldap_ldapport'];
-		$use_ldap        = $_POST['wpcasldap_useldap'];
-		$ldap_base_dn    = $_POST['wpcasldap_ldapbasedn'];
+		$cas_version     		= $_POST['wpcasldap_cas_version'];
+		$include_path    		= $_POST['wpcasldap_include_path'];
+		$server_hostname 		= $_POST['wpcasldap_server_hostname'];
+		$server_port     		= $_POST['wpcasldap_server_port'];
+		$server_path     		= $_POST['wpcasldap_server_path'];
+		$user_add        		= $_POST['wpcasldap_useradd'];
+		$user_role       		= $_POST['wpcasldap_userrole'];
+		$ldap_host       		= $_POST['wpcasldap_ldaphost'];
+		$ldap_port       		= $_POST['wpcasldap_ldapport'];
+		$use_ldap        		= $_POST['wpcasldap_useldap'];
+		$ldap_base_dn    		= $_POST['wpcasldap_ldapbasedn'];
+		$ldap_bind_dn    		= $_POST['wpcasldap_ldapbinddn'];
+		$ldap_bind_pwd    		= wp_cas_ldap_encrypt_ldapbindpwd($_POST['wpcasldap_ldapbindpwd']);
+		$ldap_map_login_attr		= $_POST['ldap_map_login_attr'];
+		$ldap_map_email_attr		= $_POST['ldap_map_email_attr'];
+		$ldap_map_alt_email_attr	= $_POST['ldap_map_alt_email_attr'];
+		$ldap_map_first_name_attr	= $_POST['ldap_map_first_name_attr'];
+		$ldap_map_last_name_attr	= $_POST['ldap_map_last_name_attr'];
+		$ldap_map_role_attr		= $_POST['ldap_map_role_attr'];
+		$ldap_map_nickname_attr		= $_POST['ldap_map_nickname_attr'];
+		$ldap_map_nicename_attr		= $_POST['ldap_map_nicename_attr'];
+		$who_can_view			= $_POST['who_can_view'];
 
 		update_site_option( 'wpcasldap_cas_version', $cas_version );
 		update_site_option( 'wpcasldap_include_path', $include_path );
@@ -55,5 +66,17 @@ function update_network_settings( ) {
 		update_site_option( 'wpcasldap_ldapport', $ldap_port );
 		update_site_option( 'wpcasldap_useldap', $use_ldap );
 		update_site_option( 'wpcasldap_ldapbasedn', $ldap_base_dn );
+		update_site_option( 'wpcasldap_ldapbinddn', $ldap_bind_dn );
+		update_site_option( 'wpcasldap_ldapbindpwd', $ldap_bind_pwd );
+		update_site_option( 'ldap_map_login_attr', $ldap_map_login_attr );
+		update_site_option( 'ldap_map_email_attr', $ldap_map_email_attr );
+		update_site_option( 'ldap_map_alt_email_attr', $ldap_map_alt_email_attr );
+		update_site_option( 'ldap_map_first_name_attr', $ldap_map_first_name_attr );
+		update_site_option( 'ldap_map_last_name_attr', $ldap_map_last_name_attr );
+		update_site_option( 'ldap_map_role_attr', $ldap_map_role_attr );
+		update_site_option( 'ldap_map_nickname_attr', $ldap_map_nickname_attr );
+		update_site_option( 'ldap_map_nicename_attr', $ldap_map_nicename_attr );
+		update_site_option( 'who_can_view', $who_can_view );
+
 	}
 }
